8542d6367dd4a1bf1aa13fb4f643f221760caefb
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-02-19 14:08:39 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -16,55 +16,58 @@ const TodoList = () => {
     }
   };
 
-  const handleDeleteTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  };
-
   const handleEditTask = (task) => {
     setEditTask(task);
     setNewTask(task.text);
   };
 
   const handleUpdateTask = () => {
-    setTasks(
-      tasks.map((task) =>
+    if (newTask.trim() !== '') {
+      const updatedTasks = tasks.map((task) =>
         task.id === editTask.id ? { ...task, text: newTask } : task
-      )
-    );
-    setEditTask(null);
-    setNewTask('');
+      );
+      setTasks(updatedTasks);
+      setEditTask(null);
+      setNewTask('');
+    }
+  };
+
+  const handleDeleteTask = (task) => {
+    const filteredTasks = tasks.filter((t) => t.id !== task.id);
+    setTasks(filteredTasks);
   };
 
   return (
     <div>
       <h1>Todo List</h1>
 
+      <div>
+        <input type="text" value={newTask} onChange={handleNewTaskChange} />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            {editTask && editTask.id === task.id ? (
+            {task === editTask ? (
               <div>
-                <input type="text" value={newTask} onChange={handleNewTaskChange} />
+                <input
+                  type="text"
+                  value={newTask}
+                  onChange={handleNewTaskChange}
+                />
                 <button onClick={handleUpdateTask}>Update</button>
               </div>
             ) : (
               <div>
                 <span>{task.text}</span>
                 <button onClick={() => handleEditTask(task)}>Edit</button>
-                <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+                <button onClick={() => handleDeleteTask(task)}>Delete</button>
               </div>
             )}
           </li>
         ))}
       </ul>
-
-      <div>
-        <input type="text" value={newTask} onChange={handleNewTaskChange} />
-        <button onClick={editTask ? handleUpdateTask : handleAddTask}>
-          {editTask ? 'Update' : 'Add'}
-        </button>
-        {editTask && <button onClick={() => setEditTask(null)}>Cancel</button>}
-      </div>
     </div>
   );
 };
